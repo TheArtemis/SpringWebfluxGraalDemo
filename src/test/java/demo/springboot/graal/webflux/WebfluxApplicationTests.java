aa91084028390af5d6e90ad83443f6298d83443c
Lorenzo Deflorian <lordef01@gmail.com>
2024-06-27 15:44:51 +0200

    Initial Commit

---
+++ src/test/java/demo/springboot/graal/webflux/WebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.springboot.graal.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
